2e8fcc9e7e31f7965894ad233c213ba75158f45b
kuzugr <monmon.syusyo@gmail.com>
2019-03-26 09:51:17 +0000

    users_controllerのテストを追加

--- spec/controllers/api/users_controller.rb
+++ spec/controllers/api/users_controller.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::SessionsController, type: :controller do
+RSpec.describe Api::V1::UsersController, type: :controller do
 
   before do
     user = User.new(id: 1, nickname: 'kuzugr', email: 'kuzugr.mail', encrypted_password: 'test', sign_in_count: 0, created_at: '2019-03-26 00:00:00', updated_at: '2019-03-26 00:00:00')
@@ -9,13 +9,13 @@ RSpec.describe Api::V1::SessionsController, type: :controller do
   end
 
 
-  describe "GET #login_state" do
-    context 'login true' do
-      before { get :login_state, params: { login_email: 'kuzugr.mail' } }
+  describe "GET #index" do
+    context 'return users info' do
+      before { get :index  }
 
       it 'return an error response' do
         expect(JSON.parse(response.code)).to eq 200
-        expect(JSON.parse(response.body)['login_state']).to eq true
+        expect(JSON.parse(response.body)['user']['nickname']).to eq 'kuzugr'
       end
     end
   end
