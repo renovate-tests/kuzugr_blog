a46e98eadb79aa37030ab8bf0cee62ac1305a6b4
kuzugr <monmon.syusyo@gmail.com>
2019-03-30 01:22:48 +0900

    広告作成のコンポーネントを作成

---
+++ client/src/app/shared/models/advertisement.ts
@@ -0,0 +1,6 @@
+export class Advertisement {
+  id: number;
+  name: string;
+  pc_link: string;
+  sp_link: string;
+}
---
+++ client/src/app/shared/services/advertisement.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { environment } from '../../../environments/environment';
+import { Advertisement } from '../models/advertisement';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AdvertisementService {
+  apiEndpoint = environment.apiEndpoint;
+
+  constructor(private http: HttpClient) { }
+
+  getAdvertisements(): Observable<Array<Advertisement>> {
+    return this.http.get<Array<Advertisement>>(`${this.apiEndpoint}/advertisements`);
+  }
+}
