b3b3c095bcf84da2b867b2e9fc82cbeb247d7f13
kuzugr <monmon.syusyo@gmail.com>
2019-03-30 23:02:09 +0900

    お問い合わせメール送信のAPIをコールするserviceを作成

---
+++ client/src/app/shared/services/contact.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { environment } from '../../../environments/environment';
+import { Contact } from '../models/contact';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ContactService {
+  apiEndpoint = environment.apiEndpoint;
+
+  constructor(private http: HttpClient) { }
+
+  sendEmail(contact: Contact): any {
+    return this.http.post<any>(`${this.apiEndpoint}/contacts/send_contact`, { contact: contact });
+  }
+}
