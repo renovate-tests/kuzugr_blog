e9a73e8759653549d51abec2832d2112fb18595a
kuzugr <monmon.syusyo@gmail.com>
2019-04-05 00:14:19 +0900

    ログイン画面にバリデーションチェックを追加

--- client/src/app/components/account/login/login.component.ts
+++ client/src/app/components/account/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ValidateForm } from '@functions/validate-forms';
 import { SessionService } from '../session.service';
 import { User } from '@models/user';
 import { CookieService } from 'ngx-cookie-service';
@@ -13,6 +14,19 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
   form: FormGroup;
   user: User;
+  gotError: boolean;
+  formErrors: {[key: string]: Array<string>} = {};
+  validationMessages = {
+    'email': {
+      'required': 'メールアドレスを入力してください。',
+      'maxlength': 'メールアドレスは100文字以内で入力してください。',
+      'email': '無効なメールアドレスの形式です。',
+    },
+    'password': {
+      'required': 'パスワードを入力してください。',
+      'maxlength': 'パスワードは50文字以内で入力してください。',
+    },
+  };
 
   constructor(private sessionService: SessionService,
               private cookieService: CookieService,
@@ -20,12 +34,14 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
     this.form = new FormGroup({
-      email: new FormControl(),
-      password: new FormControl(),
+      email: new FormControl('', [Validators.required, Validators.maxLength(100), Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.maxLength(50)]),
     });
   }
 
   onSubmit() {
+    this.gotError = false;
+    this.formErrors = {};
     if (this.form.valid) {
       this.user = this.form.value;
 
@@ -36,9 +52,11 @@ export class LoginComponent implements OnInit {
           location.href = '/';
         },
         error => {
-          // エラーメッセージを出す
+          this.gotError = true;
         },
       );
+    } else {
+      this.formErrors = ValidateForm(this.form, false, this.validationMessages);
     }
   }
 }
