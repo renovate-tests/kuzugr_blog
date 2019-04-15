ce9664c059622064d978a1e0b4a6f7a7b32e6fc5
kuzugr <monmon.syusyo@gmail.com>
2019-04-15 10:27:24 +0000

    articles_controllerのテストを追加

--- spec/controllers/api/v1/articles_controller_spec.rb
+++ spec/controllers/api/v1/articles_controller_spec.rb
@@ -3,10 +3,13 @@ require 'rails_helper'
 RSpec.describe Api::V1::ArticlesController, type: :request do
   let!(:current_user) { create(:user, id: 1) }
   let!(:article) { create(:article, user_id: current_user.id) }
+  let!(:not_published_article) { create(:article, user_id: current_user.id, published: false, created_at: '9999-12-31 23:59:59') }
   let!(:blog_informations) { create(:blog_information) }
   let!(:thumbnail) { create(:thumbnail, article_id: article.id) }
   let!(:category) { create(:category) }
   let!(:comment) { create(:comment, article_id: article.id) }
+  # 存在しないidをひとまず適当な値として定義する
+  let!(:not_exist_id) { 999999 }
 
   describe 'GET /index' do
     describe'記事がない場合' do
@@ -64,17 +67,27 @@ RSpec.describe Api::V1::ArticlesController, type: :request do
           expect(article_response['comments'][0]['content']).to eq comment.content
         end
       end
+
+      context '認証済みの場合' do
+        before do
+          allow_any_instance_of(Api::V1::ArticlesController).to receive(:published_option).and_return([true, false])
+          get '/api/v1/articles', params: { limit: 1 }
+        end
+        it '公開されていない記事を含む' do
+          article_response = JSON.parse(response.body)[0]
+          expect(article_response['id']).to eq not_published_article.id
+        end
+      end
     end
   end
 
   describe 'GET /show' do
     context '存在しないid' do
       before do
-        get '/api/v1/articles/999999'
+        get "/api/v1/articles/#{not_exist_id}"
       end
-      it 'エラーが返る' do
-        article_response = JSON.parse(response.body)
-        expect(article_response['error_code']).to eq 404
+      it '404エラーが返る' do
+        expect(response.code).to eq '404'
       end
     end
 
@@ -85,6 +98,26 @@ RSpec.describe Api::V1::ArticlesController, type: :request do
       it '正しく取得できる' do
         article_response = JSON.parse(response.body)
         expect(article_response['id']).to eq article.id
+      end
+    end
+
+    context '未認証で未公開の記事の要求をした場合' do
+      before do
+        get "/api/v1/articles/#{not_published_article.id}"
+      end
+      it '404エラーが返る' do
+        expect(response.code).to eq '404'
+      end
+    end
+
+    context '認証済みで未公開の記事の要求をした場合' do
+      before do
+        allow_any_instance_of(Api::V1::ArticlesController).to receive(:published_option).and_return([true, false])
+        get "/api/v1/articles/#{not_published_article.id}"
+      end
+      it '正しく取得できる' do
+        article_response = JSON.parse(response.body)
+        expect(article_response['id']).to eq not_published_article.id
       end
     end
   end
@@ -103,18 +136,72 @@ RSpec.describe Api::V1::ArticlesController, type: :request do
           }
       }
     end
-    before do
-      allow_any_instance_of(ApplicationController).to receive(:authenticate_user_from_token!).and_return(true)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(current_user)
-      patch "/api/v1/articles/#{params[:article][:id]}", params: params
+    context '正しいidの場合' do
+      before do
+        allow_any_instance_of(ApplicationController).to receive(:authenticate_user_from_token!).and_return(true)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(current_user)
+        patch "/api/v1/articles/#{params[:article][:id]}", params: params
+      end
+      it '更新に成功する' do
+        changed_article = Article.find(article.id)
+        expect(response.code).to eq '204'
+        expect(changed_article.title).to eq params[:article][:title]
+        expect(changed_article.mark_content).to eq params[:article][:mark_content]
+        expect(changed_article.html_content).to eq params[:article][:html_content]
+        expect(changed_article.published).to eq params[:article][:published]
+      end
     end
-    it '更新に成功する' do
-      changed_article = Article.find(article.id)
-      expect(response.code).to eq '204'
-      expect(changed_article.title).to eq params[:article][:title]
-      expect(changed_article.mark_content).to eq params[:article][:mark_content]
-      expect(changed_article.html_content).to eq params[:article][:html_content]
-      expect(changed_article.published).to eq params[:article][:published]
+
+    context '不正なidの場合' do
+      before do
+        allow_any_instance_of(ApplicationController).to receive(:authenticate_user_from_token!).and_return(true)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(current_user)
+        patch "/api/v1/articles/#{not_exist_id}", params: params
+      end
+      it '404エラーが返る' do
+        expect(response.code).to eq '404'
+      end
+    end
+
+    context '認証が通っていない場合' do
+      before do
+        patch "/api/v1/articles/#{not_exist_id}", params: params
+      end
+      it '401エラーが返る' do
+        expect(response.code).to eq '401'
+      end
+    end
+  end
+
+  describe 'POST /create' do
+    # idの被り等をなくすため既存記事を先に消しておく
+    before { Article.destroy_all }
+    let(:params) do
+      {
+        article:
+          {
+            title: 'created title',
+            mark_content: 'created mark_content',
+            html_content: 'created html_content',
+            category_id: article.category_id,
+            published: false,
+          }
+      }
+    end
+    context '正しいidの場合' do
+      before do
+        allow_any_instance_of(ApplicationController).to receive(:authenticate_user_from_token!).and_return(true)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(current_user)
+        post '/api/v1/articles', params: params
+      end
+      it '作成に成功する' do
+        created_article = Article.last
+        expect(created_article.title).to eq params[:article][:title]
+        expect(created_article.mark_content).to eq params[:article][:mark_content]
+        expect(created_article.html_content).to eq params[:article][:html_content]
+        expect(created_article.category_id).to eq params[:article][:category_id]
+        expect(created_article.published).to eq params[:article][:published]
+      end
     end
   end
 end
