66da627004f1ed5af96549b9ce47e15cf831af9b
kuzugr <monmon.syusyo@gmail.com>
2019-05-05 13:52:23 +0900

    ブログタイトル変更

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiEndpoint: 'https://kuzugr.com/api/v1',
   uploadFileEndpoint: 'https://kuzugr.com/api/v1/upload_files',
-  title: 'ロックとギターとプログラミング',
+  title: 'RubyとJavascriptと時々Rock',
 };
