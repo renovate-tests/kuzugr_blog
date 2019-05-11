dbb425a8f6873642d89284a2afe2c9c177dbdad3
kuzugr <monmon.syusyo@gmail.com>
2019-05-11 17:59:30 +0900

    rubocop対応

--- app/services/sitemap_service.rb
+++ app/services/sitemap_service.rb
@@ -7,17 +7,7 @@ class SitemapService
   def call
     create_path = Rails.root.join('sitemap.xml')
     File.open(create_path, 'w') do |file|
-      file.puts('<?xml version="1.0" encoding="UTF-8"?>')
-      file.puts('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">')
-      path_list.each do |path|
-        file.puts('  <url>')
-        file.puts("    <loc>#{ENV['CORS_ALLOW_HOST']}#{path}</loc>")
-        file.puts("    <lastmod>#{Time.zone.today.strftime('%Y-%m-%d')}</lastmod>")
-        file.puts('    <changefreq>daily</changefreq>')
-        file.puts('    <priority>1.0</priority>')
-        file.puts('  </url>')
-      end
-      file.puts('</urlset>')
+      puts_sitemap(file)
     end
     FileUtils.cp(create_path, ENV['SITEMAP_PATH'])
   end
@@ -39,5 +29,19 @@ class SitemapService
       paths << "/article/#{article_id}"
     end
     paths
+  end
+
+  def puts_sitemap(file)
+    file.puts('<?xml version="1.0" encoding="UTF-8"?>')
+    file.puts('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">')
+    path_list.each do |path|
+      file.puts('  <url>')
+      file.puts("    <loc>#{ENV['CORS_ALLOW_HOST']}#{path}</loc>")
+      file.puts("    <lastmod>#{Time.zone.today.strftime('%Y-%m-%d')}</lastmod>")
+      file.puts('    <changefreq>daily</changefreq>')
+      file.puts('    <priority>1.0</priority>')
+      file.puts('  </url>')
+    end
+    file.puts('</urlset>')
   end
 end
