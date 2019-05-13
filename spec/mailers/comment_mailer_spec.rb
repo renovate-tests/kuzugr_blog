9f1221688e2ba51cf557b799cac13a6b37c40d9d
kuzugr <monmon.syusyo@gmail.com>
2019-05-13 10:41:50 +0000

    不要な記述を削除

--- spec/mailers/comment_mailer_spec.rb
+++ spec/mailers/comment_mailer_spec.rb
@@ -4,8 +4,6 @@ RSpec.describe CommentMailer, type: :mailer do
   let(:params) { { name: 'Name', content: 'メール本文' } }
 
   describe 'メール送信' do
-    delivery = double
-    expect(delivery).to receive(:deliver_now).with(no_args)
     it 'メールの内容が正しい' do
       mail = CommentMailer.send_comment_mail(params)
       expect(mail[:subject].value).to eq Settings.mail.comment.title
