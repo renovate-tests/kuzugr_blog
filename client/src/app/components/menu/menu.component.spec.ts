d8c6d9556b87f4e9e1eabe95e0f8032db2136977
kuzugr <monmon.syusyo@gmail.com>
2019-11-27 22:07:46 +0900

    menu-componentにFormModuleを追加したのでテストにもimportする

--- client/src/app/components/menu/menu.component.spec.ts
+++ client/src/app/components/menu/menu.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { MenuComponent } from './menu.component';
 
@@ -12,7 +13,7 @@ describe('MenuComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [MenuComponent],
-      imports: [RouterTestingModule, HttpClientModule],
+      imports: [RouterTestingModule, HttpClientModule, ReactiveFormsModule],
     }).compileComponents();
   }));
 
